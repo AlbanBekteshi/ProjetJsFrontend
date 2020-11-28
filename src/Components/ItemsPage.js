05d9b9b844597e4ead3568f5a4b2e701b49a82fe
AdriendeDecker <adriendedecker@yahoo.fr>
2020-11-28 21:16:31 +0100

    Fix - affichage site

--- src/Components/ItemsPage.js
+++ src/Components/ItemsPage.js
@@ -1,14 +1,15 @@
 import {RedirectUrl} from "./Router";
-//import {API_URL} from "../utils/server";
+import {API_URL} from "../utils/server";
 import { setUserSessionData,getUserSessionData } from "../utils/session.js";
 import { setLayout } from "../utils/render.js";
 
 let page = document.querySelector("#page");
 
-"/api/users/" + userId
-fetch(API_URL + "users/" + userId, {
+//"/api/users/" + idUser
+fetch(API_URL + "users/", {
     method: "GET"
-}).then((response) => {
+})
+.then((response) => {
     if (!response.ok) {
         let fullErrorMessage = "Error code : " + response.status + ":" + response.statusText + "/nMessage : ";
         return response.text().then((errorMessage) => {
@@ -17,16 +18,16 @@ fetch(API_URL + "users/" + userId, {
         })
     }
     return response.json();
-}).then((data) => ItemsPage(data)).catch(
-)
-;
+})
+.then((data) => ItemsPage(data))
+.catch();
+
 const ItemsPage = (data) => {
     setLayout("Game Item Collection","Game Items Collection","MyCollectionPage","My footer");
     page.innerHTML = `<div id="quote"></div>`;
     console.log(data);
-    let gameItemCollectionPage = `<h5> Ma collections d'item<h5>
-        <ul class="list-group list-group-item">`;
-    let gameItemCollectionList = document.querySelector("ul");
+    //let gameItemCollectionPage = `<h5> Ma collections d'item<h5><ul class="list-group list-group-item">`;
+    //let gameItemCollectionList = document.querySelector("ul");
 };
 
 export default ItemsPage;
