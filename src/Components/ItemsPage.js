ef104bca8852a786ec6145db331e82731994e1d2
Alban <albanbekteshi97@gmail.com>
2020-12-09 00:33:18 +0100

    refactoring et commentaire

--- src/Components/ItemsPage.js
+++ src/Components/ItemsPage.js
@@ -4,7 +4,12 @@ import { setUserSessionData,getUserSessionData } from "../utils/session.js";
 import { setLayout } from "../utils/render.js";
 
 let page = document.querySelector("#page");
+
+/* jeuxSelctionner permet de choisir le jeux pour le quelle on veut voir les items
+* il est initialisé a vide car au chargement on affiche tout les items disponible
+* */
 let jeuxSelectionner = "";
+
 const ItemsPage =() =>{
     fetch(API_URL + "items", {
         method: "GET",
@@ -23,80 +28,108 @@ const ItemsPage =() =>{
     .catch();   
 }
 
+
 const onItemsPage = (data) => {
     setLayout("Game Item Collection","Game Items Collection","MyCollectionPage","My footer");
-    console.log("liste de tous les items");
-    console.log(data);
-
-
-    let RealPage =`<div class="container-fluid"><div class="row">`;
+    
+    /*totalPage est diviser en deux page différente
+    * */
+    let totalPage =`<div class="container-fluid"><div class="row">`;
+    
+    /*SelectGame est la parti gauche de totalPage qui contiens
+    * les diffenrent bouton qui nous premettes de selectionner un jeux
+    * */
     let SelectGame =`<div class=" col-2" id="itemsDivContainer">`;
+    
+    /*HomeItemsPages est la partie de droite ou les items s'afficherons en fonction du jeux choisit
+    * */
     let HomeItemsPage =`<div class="row mt-3 col-10" id="itemsDivContainer">`;
     let button;
 
-    //Fait un Distinct de tout les jeux different
-    const result = Array.from (new Set(data.map( s => s.jeu)))
+    /*Permet de selectionner da maniere distincte tout les differnt jeux disponible dans la dataBase
+    * */
+    const listDesJeux = Array.from (new Set(data.map( s => s.jeu)))
         .map(jeu =>{
             return {jeu:jeu};
         });
-    console.log(result);
+    
+    /*Creation du container pour les different Button
+    * et creation d'un bouton par jeuxDispo + ajout
+    * */
     SelectGame +=`<div class="container"><div class="btn-group-vertical">`;
 
-    result.forEach(truc=> {
+    listDesJeux.forEach(choixJeux=> {
         SelectGame +=`
-                <button type="button" class="btn btn-primary" id="${truc.jeu}">${truc.jeu} </button>`;
+                <button type="button" class="btn btn-primary" id="${choixJeux.jeu}">${choixJeux.jeu} </button>`;
     });
-    SelectGame+= `</div></div></div>`;
+    SelectGame+= `<button type="button" class="btn btn-primary" id="all">Tout afficher </button>
+            </div></div></div>`;
 
-    if(jeuxSelectionner===""){
-        data.forEach(item => {
-                HomeItemsPage+=`
-        <div class="col-lg-2 col-md-3 col-sm-6">
-                <div class="card bg-secondary p-1 mb-2">
-                    <img src="https://source.unsplash.com/300x300" class="card-img-top" alt="ItemImg">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">${item.name}</h5>
-                        <p class="card-text">
-                            <u>Jeu :</u> ${item.jeu} <br>
-                            <u>Description :</u> ${item.description} <br>
-                            <u>Prix :</u> ${item.price}<br>
-                        </p>
-                    </div>
-                </div>  
-            </div>
-        `;
-        });
+
+    if(getUserSessionData()){
+        if(jeuxSelectionner===""){
+            data.forEach(item => {
+                HomeItemsPage+=getAffichage(item);
+                HomeItemsPage+= `<p>Connecter</p>`
+            });
+        }
+        else{
+            data.forEach(item => {
+                if(item.jeu===jeuxSelectionner){
+                    HomeItemsPage+=getAffichage(item);
+                    HomeItemsPage+= `<p>Connecter</p>`
+                }
+            });
+
+        }
     }
     else{
-        data.forEach(item => {
-            if(item.jeu==jeuxSelectionner){
-                HomeItemsPage+=`
-        <div class="col-lg-2 col-md-3 col-sm-6">
-                <div class="card bg-secondary p-1 mb-2">
-                    <img src="https://source.unsplash.com/300x300" class="card-img-top" alt="ItemImg">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold">${item.name}</h5>
-                        <p class="card-text">
-                            <u>Jeu :</u> ${item.jeu} <br>
-                            <u>Description :</u> ${item.description} <br>
-                            <u>Prix :</u> ${item.price}<br>
-                        </p>
-                    </div>
-                </div>  
-            </div>
-        `;
-            }
-        });
+        if(jeuxSelectionner===""){
+            data.forEach(item => {
+                HomeItemsPage+=getAffichage(item);
+            });
+        }
+        else{
+            data.forEach(item => {
+                if(item.jeu===jeuxSelectionner){
+                    HomeItemsPage+=getAffichage(item);
+                }
+            });
 
+        }
     }
+
+
 
 
 
     HomeItemsPage+= `</div>`;
-    RealPage+= SelectGame +HomeItemsPage +`</div></div>`;
-    page.innerHTML = RealPage;
+    totalPage+= SelectGame +HomeItemsPage +`</div></div>`;
+    page.innerHTML = totalPage;
 
 
+    /*Model qui permet d'afficher les items
+    * */
+    function getAffichage(item) {
+        return `
+        <div class="col-lg-2 col-md-3 col-sm-6">
+                <div class="card bg-secondary p-1 mb-2">
+                    <img src="https://source.unsplash.com/300x300" class="card-img-top" alt="ItemImg">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">${item.name}</h5>
+                        <p class="card-text">
+                            <u>Jeu :</u> ${item.jeu} <br>
+                            <u>Description :</u> ${item.description} <br>
+                            <u>Prix :</u> ${item.price}<br>
+                        </p>
+                    </div>
+                </div>  
+            </div>
+        `;
+    }
+
+    /*Permet de faire fonctionner les boutons
+    * */
     fetch(API_URL+"items",{
         method:"GET",
     }).then((response) => {
@@ -120,11 +153,12 @@ const onItemsPage = (data) => {
         })
         })
         .catch();
-
+    button = document.getElementById("all").onclick = function () {changerJeux("")};
     function  changerJeux(nomJeux){
         jeuxSelectionner = nomJeux;
         ItemsPage();
     }
+
 };
 
 
