af45fd890e96fdb826aabc2a7c30b24118ff3107
AdriendeDecker <adriendedecker@yahoo.fr>
2020-12-11 17:15:42 +0100

    Modify - Global Chat design

--- src/Components/GlobalTchatPage.js
+++ src/Components/GlobalTchatPage.js
@@ -1,28 +1,36 @@
 import { setLayout } from "../utils/render.js";
 import { getUserSessionData } from "../utils/session.js";
 import { RedirectUrl } from "./Router.js";
+import UserList from "./UserList.js";
 let GlobalTchatSelector = document.querySelector("#page")
 const GlobalTchatPage = ()=>{
   const userCredential = getUserSessionData();
   if (!userCredential) RedirectUrl("/error", 'Resource not authorized. Please <a href="/login">login</a>.');
   else{
-  setLayout("GIC : Message","Game Items Collection",`message`,"My footer");
-  let GlobalTchathtml = `
+    UserList();
+    setLayout("GIC : Message","Game Items Collection",`Global Chat`,"My footer");
+    let GlobalTchathtml = `
+      <div class="row col-12 pt-2">
+        <ul id="Gchat" class="col-12 ml-1">
+        
+        </ul>
 
-  <ul id="Gchat"></ul>
-
-  <form>
-    <textarea rows="8" cols="80" id="message"></textarea>
-    <br />
-    <button type="submit">Send</button>
-  </form>
-  <script src="GlobalTchatPage.js"></script>
-`;
+        <form class="col-12">
+          <div class="form-group">
+            <textarea class="form-control" id="message" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-success">Envoyer</button>
+        </form>
+        <script src="GlobalTchatPage.js"></script>
+      </div>
+    
+  `;
   
   GlobalTchatSelector.innerHTML = GlobalTchathtml;
   document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
-    let message = document.querySelector('#message').value;
+    let username = userCredential.username;
+    let message = username+" : "+ document.querySelector('#message').value;
     connection.send(message);
     document.querySelector('#message').value = '';
   });}
@@ -45,7 +53,6 @@ connection.onerror = error => {
 };
 
 connection.onmessage = event => {
-  console.log('received', event.data);
   let li = document.createElement('li');
   li.innerText = event.data;
   document.querySelector('#Gchat').append(li);
